1f858ed59b03eeb75432a83e637fa50af5c6e3fb
JinGeun Lee <zealotjin@gmail.com>
2014-11-01 15:10:16 -0700

    Changed file structure

---
+++ iTap/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  iTap
+//
+
+//  Copyright (c) 2014 KJJ Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ iTap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iTap
+//
+//  Created by JinGeun Lee on 11/1/14.
+//  Copyright (c) 2014 KJJ Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
