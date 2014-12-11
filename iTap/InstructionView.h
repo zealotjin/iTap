8733654365bb220695d38385ab9ee1bfaafe70a9
June Woo Suk <junewoo.suk@gmail.com>
2014-12-11 01:47:43 -0800

    instruction page

---
+++ iTap/InstructionView.h
@@ -0,0 +1,13 @@
+//
+//  InstructionView.h
+//  iTap
+//
+//  Created by June Woo Suk on 12/11/14.
+//  Copyright (c) 2014 KJJ Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstructionView : UIView
+
+@end
